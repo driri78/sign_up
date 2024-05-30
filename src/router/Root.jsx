535880f5f5afa9fe671eb6072c9748293e498fbe
kdw <ksgkdw78@naver.com>
2024-05-30 17:23:48 +0900

    router

---
+++ src/router/Root.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+
+const loding = <div>로딩중.......</div>;
+const SignUpPage = lazy(() =>
+  import("../account/sign/sign_up/pages/SignUpPage")
+);
+
+const Root = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "",
+        element: <Suspense fallback={loding}></Suspense>,
+      },
+      {
+        path: "account",
+        children: [
+          {
+            path: "sign-up",
+            element: (
+              <Suspense fallback={loding}>
+                <SignUpPage />
+              </Suspense>
+            ),
+          },
+          {
+            path: "sign-in",
+            element: <Suspense fallback={loding}></Suspense>,
+          },
+        ],
+      },
+    ],
+  },
+  {},
+]);
+
+export default Root;
